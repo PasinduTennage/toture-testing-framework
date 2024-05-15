d0628eb415fd8e6816102118b063259eac902b41
“Pasindu <pasindunivanthakatennage@gmail.com>
2024-05-15 21:07:14 +0200

    commenting

--- dummy/configuration/config.go
+++ dummy/configuration/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 /*
-	config.go defines the structs and methods to pass the configuration file, that contains the IP:ports of each torture
+	config.go defines the structs and methods to pass the configuration file, that contains the IP:ports of each dummy replica
 */
 
 type ReplicaInstance struct {
