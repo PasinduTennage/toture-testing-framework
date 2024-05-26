6ae1ae1d27ac631d470c4c8837d9ed1f86cfb133
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-05-26 21:20:25 +0200

    minor edits

--- torture/util/network_util.go
+++ torture/util/network_util.go
@@ -38,12 +38,12 @@ func GetProcessID(port string) int {
 func RunCommand(name string, arg []string) error {
 	cmd := exec.Command(name, arg...)
 	if cmd.Err != nil {
-		fmt.Println("Error running command " + name + strings.Join(arg, " ") + " " + cmd.Err.Error() + "\n")
+		fmt.Println("Error running command " + name + " " + strings.Join(arg, " ") + " " + cmd.Err.Error() + "\n")
 		return cmd.Err
 	}
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println("Error running command " + name + strings.Join(arg, " ") + " " + err.Error() + "\n")
+		fmt.Println("Error running command " + name + " " + strings.Join(arg, " ") + " " + err.Error() + "\n")
 		return err
 	} else {
 		fmt.Println("Command successful " + name + " " + strings.Join(arg, " ") + "\n")
