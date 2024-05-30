1c2499c571d51d0688a3e7087e46a3314ce741cd
“Pasindu <pasindunivanthakatennage@gmail.com>
2024-05-30 20:43:57 +0200

    local netem simulation done! all codes checked!

--- torture/controller_frontend/controller_user.go
+++ torture/controller_frontend/controller_user.go
@@ -12,10 +12,16 @@ import (
 func StartAttack(nodes []torture.Attacker) {
 	start_time := time.Now()
 	for time.Now().Sub(start_time) < 120*time.Second {
+		// delay
 		for _, node := range nodes {
-			node.LossPackets(rand.Intn(20), true)
+			node.DelayPackets(rand.Intn(20), true)
 		}
 		time.Sleep(2 * time.Second)
+		for _, node := range nodes {
+			node.DelayPackets(rand.Intn(20), false)
+		}
+		time.Sleep(2 * time.Second)
+		// loss
 		for _, node := range nodes {
 			node.LossPackets(rand.Intn(20), true)
 		}
@@ -23,6 +29,54 @@ func StartAttack(nodes []torture.Attacker) {
 		for _, node := range nodes {
 			node.LossPackets(rand.Intn(20), false)
 		}
+		time.Sleep(2 * time.Second)
+
+		// duplicate
+		for _, node := range nodes {
+			node.DuplicatePackets(rand.Intn(20), true)
+		}
+		time.Sleep(2 * time.Second)
+		for _, node := range nodes {
+			node.DuplicatePackets(rand.Intn(20), false)
+		}
+		time.Sleep(2 * time.Second)
+
+		// reorder
+		for _, node := range nodes {
+			node.ReorderPackets(rand.Intn(20), true)
+		}
+		time.Sleep(2 * time.Second)
+		for _, node := range nodes {
+			node.ReorderPackets(rand.Intn(20), false)
+		}
+		time.Sleep(2 * time.Second)
+
+		// corrupt
+		for _, node := range nodes {
+			node.CorruptPackets(rand.Intn(20), true)
+		}
+		time.Sleep(2 * time.Second)
+		for _, node := range nodes {
+			node.CorruptPackets(rand.Intn(20), false)
+		}
+		time.Sleep(2 * time.Second)
+
+		// pause
+		for _, node := range nodes {
+			node.Pause(true)
+		}
+		time.Sleep(2 * time.Second)
+		for _, node := range nodes {
+			node.Pause(false)
+		}
+		time.Sleep(2 * time.Second)
+
+		// resetAll
+		for _, node := range nodes {
+			node.ResetAll()
+		}
+		time.Sleep(2 * time.Second)
+
 	}
 	for _, node := range nodes {
 		node.CleanUp()
