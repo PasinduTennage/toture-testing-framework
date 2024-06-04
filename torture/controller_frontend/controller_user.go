0fd031d2141a81ce3501c93f55138ad8673ab5d9
“Pasindu <pasindunivanthakatennage@gmail.com>
2024-06-04 18:06:47 +0200

    local host tests all pass!

--- torture/controller_frontend/controller_user.go
+++ torture/controller_frontend/controller_user.go
@@ -11,65 +11,65 @@ import (
 
 func StartAttack(nodes []torture.Attacker) {
 	start_time := time.Now()
-	for time.Now().Sub(start_time) < 120*time.Second {
-		// delay
+	for time.Now().Sub(start_time) < 20*time.Second {
+		//delay
 		for _, node := range nodes {
 			node.DelayPackets(rand.Intn(20))
 		}
 		time.Sleep(2 * time.Second)
-		for _, node := range nodes {
-			node.DelayPackets(0)
-		}
-		time.Sleep(2 * time.Second)
-		// loss
+		//for _, node := range nodes {
+		//	node.DelayPackets(0)
+		//}
+		//time.Sleep(2 * time.Second)
+		//loss
 		for _, node := range nodes {
 			node.LossPackets(rand.Intn(20))
 		}
 		time.Sleep(2 * time.Second)
-		for _, node := range nodes {
-			node.LossPackets(0)
-		}
-		time.Sleep(2 * time.Second)
+		//for _, node := range nodes {
+		//	node.LossPackets(0)
+		//}
+		//time.Sleep(2 * time.Second)
 
 		// duplicate
 		for _, node := range nodes {
-			node.DuplicatePackets(rand.Intn(20))
+			node.DuplicatePackets(90)
 		}
 		time.Sleep(2 * time.Second)
-		for _, node := range nodes {
-			node.DuplicatePackets(0)
-		}
-		time.Sleep(2 * time.Second)
+		//for _, node := range nodes {
+		//	node.DuplicatePackets(0)
+		//}
+		//time.Sleep(2 * time.Second)
 
-		// reorder
+		//reorder
 		for _, node := range nodes {
 			node.ReorderPackets(rand.Intn(20))
 		}
 		time.Sleep(2 * time.Second)
-		for _, node := range nodes {
-			node.ReorderPackets(0)
-		}
-		time.Sleep(2 * time.Second)
+		//for _, node := range nodes {
+		//	node.ReorderPackets(0)
+		//}
+		//time.Sleep(2 * time.Second)
 
 		// corrupt
 		for _, node := range nodes {
 			node.CorruptPackets(20)
 		}
 		time.Sleep(2 * time.Second)
-		for _, node := range nodes {
-			node.CorruptPackets(0)
-		}
-		time.Sleep(2 * time.Second)
+		//for _, node := range nodes {
+		//	node.CorruptPackets(0)
+		//}
+		//time.Sleep(2 * time.Second)
 
 		//pause
 		for _, node := range nodes {
 			node.Pause(true)
 		}
 		time.Sleep(2 * time.Second)
-		for _, node := range nodes {
-			node.Pause(false)
-		}
-		time.Sleep(2 * time.Second)
+		//for _, node := range nodes {
+		//	node.Pause(false)
+		//}
+		//time.Sleep(2 * time.Second)
 
 		//resetAll
 		for _, node := range nodes {
