3de981b72c47831f333c0d2e5c4bd77a36f30a14
“Pasindu <pasindunivanthakatennage@gmail.com>
2024-06-04 18:12:19 +0200

    local host tests all pass!

--- torture/controller_frontend/controller_user.go
+++ torture/controller_frontend/controller_user.go
@@ -17,65 +17,65 @@ func StartAttack(nodes []torture.Attacker) {
 			node.DelayPackets(rand.Intn(20))
 		}
 		time.Sleep(2 * time.Second)
-		//for _, node := range nodes {
-		//	node.DelayPackets(0)
-		//}
-		//time.Sleep(2 * time.Second)
-		//loss
 		for _, node := range nodes {
-			node.LossPackets(rand.Intn(20))
+			node.DelayPackets(0)
 		}
 		time.Sleep(2 * time.Second)
+		//loss
+		//for _, node := range nodes {
+		//	node.LossPackets(rand.Intn(20))
+		//}
+		//time.Sleep(2 * time.Second)
 		//for _, node := range nodes {
 		//	node.LossPackets(0)
 		//}
 		//time.Sleep(2 * time.Second)
 
 		// duplicate
-		for _, node := range nodes {
-			node.DuplicatePackets(90)
-		}
-		time.Sleep(2 * time.Second)
+		//for _, node := range nodes {
+		//	node.DuplicatePackets(90)
+		//}
+		//time.Sleep(2 * time.Second)
 		//for _, node := range nodes {
 		//	node.DuplicatePackets(0)
 		//}
 		//time.Sleep(2 * time.Second)
 
 		//reorder
-		for _, node := range nodes {
-			node.ReorderPackets(rand.Intn(20))
-		}
-		time.Sleep(2 * time.Second)
+		//for _, node := range nodes {
+		//	node.ReorderPackets(rand.Intn(20))
+		//}
+		//time.Sleep(2 * time.Second)
 		//for _, node := range nodes {
 		//	node.ReorderPackets(0)
 		//}
 		//time.Sleep(2 * time.Second)
 
 		// corrupt
-		for _, node := range nodes {
-			node.CorruptPackets(20)
-		}
-		time.Sleep(2 * time.Second)
+		//for _, node := range nodes {
+		//	node.CorruptPackets(20)
+		//}
+		//time.Sleep(2 * time.Second)
 		//for _, node := range nodes {
 		//	node.CorruptPackets(0)
 		//}
 		//time.Sleep(2 * time.Second)
 
 		//pause
-		for _, node := range nodes {
-			node.Pause(true)
-		}
-		time.Sleep(2 * time.Second)
+		//for _, node := range nodes {
+		//	node.Pause(true)
+		//}
+		//time.Sleep(2 * time.Second)
 		//for _, node := range nodes {
 		//	node.Pause(false)
 		//}
 		//time.Sleep(2 * time.Second)
 
 		//resetAll
-		for _, node := range nodes {
-			node.ResetAll()
-		}
-		time.Sleep(2 * time.Second)
+		//for _, node := range nodes {
+		//	node.ResetAll()
+		//}
+		//time.Sleep(2 * time.Second)
 
 	}
 	for _, node := range nodes {
