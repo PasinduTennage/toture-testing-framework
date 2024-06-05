f3c055e4f584a309a41601954b2e98661eb1d514
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-06-05 23:17:11 +0200

    minor edits

--- dummy/run/local-run.sh
+++ dummy/run/local-run.sh
@@ -12,6 +12,14 @@ nohup ./${dummy_path} --name 3 --ui &
 nohup ./${dummy_path} --name 4 --ui &
 nohup ./${dummy_path} --name 5 --ui &
 
+sleep 10
+
+xdg-open "http://localhost:63342/toture-testing-consensus/dummy/run/index.html/?port=10100"
+xdg-open "http://localhost:63342/toture-testing-consensus/dummy/run/index.html/?port=20100"
+xdg-open "http://localhost:63342/toture-testing-consensus/dummy/run/index.html/?port=30100"
+xdg-open "http://localhost:63342/toture-testing-consensus/dummy/run/index.html/?port=40100"
+xdg-open "http://localhost:63342/toture-testing-consensus/dummy/run/index.html/?port=50100"
+
 echo "Started 5 dummy replicas"
 
 sleep ${timeout}
