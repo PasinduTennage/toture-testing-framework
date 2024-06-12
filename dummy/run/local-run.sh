a0e336dbeaf2f59eafb9184d2ca61354fa7d67fb
“Pasindu <pasindunivanthakatennage@gmail.com>
2024-06-12 21:38:30 +0200

    all in; local run.sh and local run tortre ,sh not working because of permision issues, local and remote netem based ones to be run (all codes checked) with new changes alreayd applied

--- dummy/run/local-run.sh
+++ dummy/run/local-run.sh
@@ -30,13 +30,7 @@ xdg-open "http://localhost:63342/toture-testing-consensus/dummy/run/index.html/?
 
 echo "Started 5 dummy replicas"
 
-nohup ./${torture_path} --name 11 --config torture/configuration/local-config.cfg  --replicaConfig torture/configuration/local_consensus_config/11.cfg --debugOn --debugLevel 2 --attacker localNetEm > logs/torture/11.log &
-nohup ./${torture_path} --name 12 --config torture/configuration/local-config.cfg  --replicaConfig torture/configuration/local_consensus_config/12.cfg --debugOn --debugLevel 2 --attacker localNetEm > logs/torture/12.log &
-nohup ./${torture_path} --name 13 --config torture/configuration/local-config.cfg  --replicaConfig torture/configuration/local_consensus_config/13.cfg --debugOn --debugLevel 2 --attacker localNetEm > logs/torture/13.log &
-nohup ./${torture_path} --name 14 --config torture/configuration/local-config.cfg  --replicaConfig torture/configuration/local_consensus_config/14.cfg --debugOn --debugLevel 2 --attacker localNetEm > logs/torture/14.log &
-nohup ./${torture_path} --name 15 --config torture/configuration/local-config.cfg  --replicaConfig torture/configuration/local_consensus_config/15.cfg --debugOn --debugLevel 2 --attacker localNetEm > logs/torture/15.log &
-
-./${torture_path} --name 16 --config torture/configuration/local-config.cfg  --debugOn --debugLevel 2 --attacker localNetEm --isController > logs/torture/16.log
+sudo /bin/bash dummy/run/local-run-torture.sh
 
 sleep 10
 
