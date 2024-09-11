b2e509853fa68e8cdcd277344481c75f43119c61
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-09-11 19:41:19 +0200

    build fixes

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ sudo apt install -y protobuf-compiler
 /usr/local/go/bin/go get github.com/shirou/gopsutil/v3/...
 /usr/local/go/bin/go get fyne.io/fyne/v2
 export PATH=$PATH:$(go env GOPATH)/bin
-source ~/.bashrc
+export PATH=$PATH:/usr/local/go/bin
 /usr/local/go/bin/go mod vendor
 /usr/local/go/bin/go mod tidy
 protoc --go_out=. --go_opt=paths=source_relative consenbench/common/message.proto
