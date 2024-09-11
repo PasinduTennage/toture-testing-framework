7a2789ab4460abf03f62739404ba5c1b3f72968c
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-09-11 13:47:28 +0200

    adding dependecies

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 pwd
-go get -u github.com/golang/protobuf/protoc-gen-go
-go get github.com/shirou/gopsutil/v3/...
-go get fyne.io/fyne/v2
+/usr/local/go/bin/go get -u github.com/golang/protobuf/protoc-gen-go
+/usr/local/go/bin/go get github.com/shirou/gopsutil/v3/...
+/usr/local/go/bin/go get fyne.io/fyne/v2
 /usr/local/go/bin/go mod vendor
 /usr/local/go/bin/go mod tidy
 protoc --go_out=. --go_opt=paths=source_relative consenbench/common/message.proto
