89d99df77ca467c4c25ba5a9fb8023549d3429e6
PasinduTennage <pasindunivanthakatennage@gmail.com>
2024-09-12 15:13:36 +0200

    nits

--- consenbench/controller/controller_handler.go
+++ consenbench/controller/controller_handler.go
@@ -8,7 +8,7 @@ import (
 func (c *Controller) Handle(msg *common.ControlMsg, node int) {
 	if msg.OperationType == int32(common.GetOperationCodes().Stats) {
 		c.logger.Debug(fmt.Sprintf("Received stats %v from %v", msg.FloatArgs, node), 0)
-		c.Nodes[node-1].UpdateStats(msg.FloatArgs)
+		c.Nodes[node-2].UpdateStats(msg.FloatArgs)
 	} else {
 		c.logger.Debug(fmt.Sprintf("Unknown operation type %v", msg.OperationType), 0)
 	}
