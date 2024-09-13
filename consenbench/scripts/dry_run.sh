e2ecf6a4c87cb49d4798d8db6a7b2d6c5104ba6a
PasinduTennage <pasindunivanthakatennage@gmail.com>
2024-09-13 20:53:24 +0200

    nits

--- consenbench/scripts/dry_run.sh
+++ consenbench/scripts/dry_run.sh
@@ -2,4 +2,4 @@ git pull origin main
 /bin/bash build.sh
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type bootstrap --log_file /home/pasindu/bench/log.log
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type copy      --log_file /home/pasindu/bench/log.log --consensus_algorithm baxos
-./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type run       --log_file /home/pasindu/bench/log.log --consensus_algorithm baxos  --attack_duration 60
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type run       --log_file /home/pasindu/bench/log.log --consensus_algorithm baxos  --attack_duration 60 --attack basic
