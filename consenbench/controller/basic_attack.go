a8ed6ea2a12e36466c7857a707a15deeb61ae0cf
PasinduTennage <pasindunivanthakatennage@gmail.com>
2024-09-13 21:20:29 +0200

    mock attack

--- consenbench/controller/basic_attack.go
+++ consenbench/controller/basic_attack.go
@@ -14,6 +14,23 @@ func NewBasicAttack() *BasicAttack {
 
 func (a *BasicAttack) Attack(nodes []*AttackNode, links [][]*AttackLink, oracle *LeaderOracle, duration int) {
 	fmt.Printf("Running basic attack for %v seconds\n", duration)
-	time.Sleep(time.Duration(duration) * time.Second)
+	start_time := time.Now()
+
+	for time.Now().Sub(start_time).Seconds() < float64(duration-5) {
+		for _, node := range nodes {
+			node.Slowdown()
+		}
+		time.Sleep(1 * time.Second)
+		for i := 0; i < len(links); i++ {
+			for j := 0; j < len(links[i]); j++ {
+				if i == j {
+					continue
+				}
+				links[i][j].SetLoss(0)
+			}
+		}
+		time.Sleep(1 * time.Second)
+	}
+
 	fmt.Print("Basic attack complete\n")
 }
