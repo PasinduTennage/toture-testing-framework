396ff1d5eaad7598ec56ec38282e2ae50ce86baf
PasinduTennage <pasindunivanthakatennage@gmail.com>
2024-09-14 19:15:54 +0200

    adding client functions

--- consenbench/client/client_handler.go
+++ consenbench/client/client_handler.go
@@ -13,22 +13,31 @@ func (c *Client) Handle(msg *common.ControlMsg) {
 		panic("Received Stats signal from controller")
 	} else if int(msg.OperationType) == common.GetOperationCodes().Kill {
 		c.logger.Debug("Received Kill signal from controller", 3)
+		c.Kill()
 	} else if int(msg.OperationType) == common.GetOperationCodes().Slowdown {
 		c.logger.Debug("Received Slowdown signal from controller", 3)
+		c.SlowDown()
 	} else if int(msg.OperationType) == common.GetOperationCodes().Pause {
 		c.logger.Debug("Received Pause signal from controller", 3)
+		c.Pause()
 	} else if int(msg.OperationType) == common.GetOperationCodes().Continue {
 		c.logger.Debug("Received Continue signal from controller", 3)
+		c.Continue()
 	} else if int(msg.OperationType) == common.GetOperationCodes().SetSkew {
 		c.logger.Debug("Received SetSkew signal from controller", 3)
+		c.SetSkew(msg.FloatArgs[0])
 	} else if int(msg.OperationType) == common.GetOperationCodes().SetDrift {
 		c.logger.Debug("Received SetDrift signal from controller", 3)
+		c.SetDrift(msg.FloatArgs[0])
 	} else if int(msg.OperationType) == common.GetOperationCodes().SetDelay {
 		c.logger.Debug("Received SetDelay signal from controller", 3)
+		c.SetDelay(msg.FloatArgs[0])
 	} else if int(msg.OperationType) == common.GetOperationCodes().SetLoss {
 		c.logger.Debug("Received SetLoss signal from controller", 0)
+		c.SetLoss(msg.FloatArgs[0])
 	} else if int(msg.OperationType) == common.GetOperationCodes().SetBandwidth {
 		c.logger.Debug("Received SetBandwidth signal from controller", 3)
+		c.SetBandwidth(msg.FloatArgs[0])
 	} else if int(msg.OperationType) == common.GetOperationCodes().SetPorts {
 		c.logger.Debug("Received SetPorts signal from controller", 3)
 		c.SetPorts(msg)
