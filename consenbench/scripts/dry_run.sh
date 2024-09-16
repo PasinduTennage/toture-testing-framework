b8ccf9b756c47e32773fb03b4c1a6a6cfcbb615a
PasinduTennage <pasindunivanthakatennage@gmail.com>
2024-09-16 14:16:47 +0200

    nits

--- consenbench/scripts/dry_run.sh
+++ consenbench/scripts/dry_run.sh
@@ -1,5 +1,5 @@
 git pull origin main
 /bin/bash build.sh
-./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type bootstrap  --consensus_algorithm baxos  --attack_duration 60 --attack noop --device enp1s0
-./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type copy       --consensus_algorithm baxos  --attack_duration 60 --attack noop --device enp1s0
-./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type run        --consensus_algorithm baxos  --attack_duration 60 --attack noop --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type bootstrap  --consensus_algorithm baxos  --attack_duration 60 --attack basic --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type copy       --consensus_algorithm baxos  --attack_duration 60 --attack basic --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type run        --consensus_algorithm baxos  --attack_duration 60 --attack basic --device enp1s0
