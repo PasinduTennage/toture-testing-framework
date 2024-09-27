b420a54afbcb28c0559cdc11216ec8fa2444b2a0
PasinduTennage <pasindunivanthakatennage@gmail.com>
2024-09-27 19:19:22 +0200

    basic attack modified' all files checked

--- consenbench/controller/attack_basic.go
+++ consenbench/controller/attack_basic.go
@@ -24,34 +24,34 @@ func (a *BasicAttack) Attack(nodes []*AttackNode, links [][]*AttackLink, oracle 
 		links[0][1].SetBandwidth(1)
 		links[0][2].SetBandwidth(2)
 
-		//links[1][2].SetBandwidth(3)
-		//links[1][3].SetBandwidth(4)
-		//
-		//links[2][3].SetBandwidth(5)
-		//links[2][4].SetBandwidth(6)
-		//
-		//links[3][4].SetBandwidth(7)
-		//links[3][0].SetBandwidth(8)
-		//
-		//links[4][0].SetBandwidth(9)
-		//links[4][1].SetBandwidth(10)
+		links[1][2].SetBandwidth(3)
+		links[1][3].SetBandwidth(4)
+
+		links[2][3].SetBandwidth(5)
+		links[2][4].SetBandwidth(6)
+
+		links[3][4].SetBandwidth(7)
+		links[3][0].SetBandwidth(8)
+
+		links[4][0].SetBandwidth(9)
+		links[4][1].SetBandwidth(10)
 
 		time.Sleep(3 * time.Second)
 
 		links[0][1].SetBandwidth(10000000)
 		links[0][2].SetBandwidth(10000000)
 
-		//links[1][2].SetBandwidth(10000000)
-		//links[1][3].SetBandwidth(10000000)
-		//
-		//links[2][3].SetBandwidth(10000000)
-		//links[2][4].SetBandwidth(10000000)
-		//
-		//links[3][4].SetBandwidth(10000000)
-		//links[3][0].SetBandwidth(10000000)
-		//
-		//links[4][0].SetBandwidth(10000000)
-		//links[4][1].SetBandwidth(10000000)
+		links[1][2].SetBandwidth(10000000)
+		links[1][3].SetBandwidth(10000000)
+
+		links[2][3].SetBandwidth(10000000)
+		links[2][4].SetBandwidth(10000000)
+
+		links[3][4].SetBandwidth(10000000)
+		links[3][0].SetBandwidth(10000000)
+
+		links[4][0].SetBandwidth(10000000)
+		links[4][1].SetBandwidth(10000000)
 
 		time.Sleep(1 * time.Second)
 		//fmt.Printf("The leader order is %v\n", oracle.GetTopNLeaders())
