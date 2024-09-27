9608be76c5caf820b07bbd418f0d01f2714475bc
PasinduTennage <pasindunivanthakatennage@gmail.com>
2024-09-27 18:42:01 +0200

    attack_basic changes; all files in

--- consenbench/controller/attack_basic.go
+++ consenbench/controller/attack_basic.go
@@ -23,14 +23,35 @@ func (a *BasicAttack) Attack(nodes []*AttackNode, links [][]*AttackLink, oracle 
 	for time.Now().Sub(start_time).Seconds() < float64(duration-5) {
 		links[0][1].SetDelay(200)
 		links[0][2].SetDelay(300)
-		links[0][3].SetDelay(400)
-		links[0][4].SetDelay(500)
+
+		links[1][2].SetDelay(400)
+		links[1][3].SetDelay(500)
+
+		links[2][3].SetDelay(600)
+		links[2][4].SetDelay(700)
+
+		links[3][4].SetDelay(800)
+		links[3][0].SetDelay(900)
+
+		links[4][0].SetDelay(1000)
+		links[4][1].SetDelay(1100)
 
 		time.Sleep(3 * time.Second)
+
 		links[0][1].SetDelay(0)
 		links[0][2].SetDelay(0)
-		links[0][3].SetDelay(0)
-		links[0][4].SetDelay(0)
+
+		links[1][2].SetDelay(0)
+		links[1][3].SetDelay(0)
+
+		links[2][3].SetDelay(0)
+		links[2][4].SetDelay(0)
+
+		links[3][4].SetDelay(0)
+		links[3][0].SetDelay(0)
+
+		links[4][0].SetDelay(0)
+		links[4][1].SetDelay(0)
 
 		time.Sleep(1 * time.Second)
 		//fmt.Printf("The leader order is %v\n", oracle.GetTopNLeaders())
